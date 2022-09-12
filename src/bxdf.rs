f3a2a65cf8e5b229260c6f5eed5228097df36b55
Maddog241 <1248826874@qq.com>
2022-09-12 11:28:34 +0800

    implement lambertian brdf

--- src/bxdf.rs
+++ src/bxdf.rs
@@ -1,7 +1,10 @@
+pub mod lambertian;
+
+
 use crate::spectrum::Spectrum;
 use cgmath::{Point2, Vector3};
 
 pub trait Bxdf {
-    fn f(wi: &Vector3<f64>, wo: &Vector3<f64>) -> Spectrum;
-    fn sample_f(wi: &Vector3<f64>, wo: &Vector3<f64>, sample: Point2<f64>, pdf: &mut f64) -> Spectrum;
+    fn f(&self, wo: &Vector3<f64>, wi: &Vector3<f64>) -> Spectrum;
+    fn sample_f(&self, wo: &Vector3<f64>, wi: &mut Vector3<f64>, sample: Point2<f64>, pdf: &mut f64) -> Spectrum;
 }
---
+++ src/bxdf/lambertian.rs
@@ -0,0 +1,25 @@
+use super::Bxdf;
+use super::Spectrum;
+use cgmath::{Vector3, Point2};
+use std::f64::consts::PI;
+
+const INV_PI: f64 = 1.0 / PI;
+
+pub struct LambertianReflection {
+    reflectance: Spectrum,    
+}
+
+impl Bxdf for LambertianReflection {
+    fn f(&self, _wo: &Vector3<f64>, _wi: &Vector3<f64>) -> Spectrum {
+        self.reflectance * INV_PI
+    }
+
+    fn sample_f(&self, _wo: &Vector3<f64>, wi: &mut Vector3<f64>, sample: Point2<f64>, pdf: &mut f64) -> Spectrum {
+        *pdf = INV_PI / 2.0;
+        let theta = sample[0].acos();
+        let phi = sample[1] * 2.0 * PI;
+        *wi = Vector3::new(phi.cos(), phi.sin(), theta.sin());
+
+        self.reflectance *  INV_PI
+    }
+}
